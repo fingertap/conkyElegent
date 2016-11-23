bdf265bcfe5036ad3054ffe259933ec60b3c956e
Han <zh950713@gmail.com>
2016-11-23 11:58:21 +0800

    Use thinkpage API to determine the city

--- updateConfig.py
+++ updateConfig.py
@@ -7,26 +7,27 @@ if __name__ == '__main__':
     WEATHER = 'NA'
     import requests
     try:
-        details = requests.get('http://ipinfo.io/json').json()
-        CITY=details['city']
-        COUNTRY=details['country']
-        LATITUDE=details['loc'].split(',')[0]
-        LONGITUDE=details['loc'].split(',')[1]
+        ip = requests.get('http://ipinfo.io/json').json()['ip']
+        url = 'https://api.thinkpage.cn/v3/location/search.json?key=%s&q=%s&language=en'%(KEY, ip)
+        details = requests.get(url).json()['results'][0]
+        CITY_ID = details['id']
+        CITY = details['name']
+        COUNTRY = details['country']
     except:
         pass
-        #print('Location fetch fail')
-#    try:
-    URL = 'https://api.thinkpage.cn/v2/weather/now.json?city=%s&language=en&unit=c&key=%s'%(CITY.lower(), KEY)
-    x = requests.get(URL, timeout=5).json()
-    TEMPERATURE = x['weather'][0]['now']['temperature']
-    WEATHER = x['weather'][0]['now']['text']
-    cur_weather_code = x['weather'][0]['now']['code']
-    TARGET_FIG = '/home/han/.conky/weather/%s.png'%cur_weather_code
-#    except:
-#        pass
+    try:
+        # try get the city name with the location information
+        URL = 'https://api.thinkpage.cn/v2/weather/now.json?city=%s&language=en&unit=c&key=%s'%(CITY_ID, KEY)
+        x = requests.get(URL, timeout=5).json()
+        TEMPERATURE = x['weather'][0]['now']['temperature']
+        WEATHER = x['weather'][0]['now']['text']
+        cur_weather_code = x['weather'][0]['now']['code']
+        TARGET_FIG = '/home/han/.conky/weather/%s.png'%cur_weather_code
+    except:
+        pass
         #print('Weather fetch fail')
     try:
-        f = open('CONFIGS.py', 'w')
+        f = open('/home/han/.conky/CONFIGS.py', 'w')
         f.write('CITY=\'%s\'\n'%CITY)
         f.write('COUNTRY=\'%s\'\n'%COUNTRY)
         f.write('TARGET_FIG=\'%s\'\n'%TARGET_FIG)
